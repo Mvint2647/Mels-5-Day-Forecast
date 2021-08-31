614a7a3a27e887ba37aa60ed4b2ba73afaa0a01a
Mel Vint <melvint@Mels-MacBook-Pro.local>
2021-08-31 18:00:12 -0400

    updating look/function

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,26 @@
 let forecast = {
     "apiKey": "55f1b228797ce6d2806f07ff2d530373",
     fetchForecast: function(city) {
-        fetch("https://api.openweathermap.org/data/2.5/forecast?q=
-         + city 
-         + "&units=appid="
-         + this.apiKey
-    
+        fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
         .then((response) => response.json())
         .then((data) => console.log(data));
     },
     displayForecast: function(data) {
         var { name } = data;
-        var { icon, description } = data.forecast [0];
+        var { icon, des } = data.forecast [0];
         var { temp, humidity } = data.main;
         var { speed } = data.wind;
-        console.log(name, icon, description,temp,humidity,speed);
+        console.log(name, icon, des,temp,humidity,speed);
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".city").innerText = "Forecast in " + name;
         document.querySelector(".temperature").innerText = temp + "°F";
-        document.querySelector(".description").innerText = description;
-        document.querySelector
+        document.querySelector(".des").innerText = des;
+        document.querySelector(".wind").innerText = "Wind speed : " + speed + " mp/h";
         document.querySelector(".icon").scr = "https://openweatheermap.org/img/wn/" + icon+ "@2x.png"
     }
 };
