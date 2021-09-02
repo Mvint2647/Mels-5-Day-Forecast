75e34fefce17fbef6bc201dfdb5f092b83b18ae2
Mel Vint <melvint@Mels-MacBook-Pro.local>
2021-09-01 22:37:58 -0400

    reworking

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,9 @@
-let forecast = {
-    "apiKey": "55f1b228797ce6d2806f07ff2d530373",
-    fetchForecast: function(city) {
-        fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=metric&appid=" +
-        this.apiKey
-    )
-        .then((response) => response.json())
-        .then((data) => console.log(data));
-    },
-    displayForecast: function(data) {
-        var { name } = data;
-        var { icon, des } = data.forecast [0];
-        var { temp, humidity } = data.main;
-        var { speed } = data.wind;
-        console.log(name, icon, des,temp,humidity,speed);
-        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector(".city").innerText = "Forecast in " + name;
-        document.querySelector(".temperature").innerText = temp + "°F";
-        document.querySelector(".des").innerText = des;
-        document.querySelector(".wind").innerText = "Wind speed : " + speed + " mp/h";
-        document.querySelector(".icon").scr = "https://openweatheermap.org/img/wn/" + icon+ "@2x.png"
-    }
-};
+let city = $("#searchTerm").val();
+const apiKey = "&appid=48e10b003671672408ff6f50d6f5af92";
+let date = new Date();
+
+
+
+$("#searchBtn").on("click", function() {
+
+}
