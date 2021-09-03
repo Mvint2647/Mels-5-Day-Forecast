b27327d15c9b0f3e3ec75e9c4dd64793d6ac3102
Mel Vint <melvint@Mels-MacBook-Pro.local>
2021-09-03 17:51:36 -0400

    javascript updating

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,42 @@ let date = new Date();
 $("#searchBtn").on("click", function() {
 
 }
+city = $("#searchTerm").val();
+$('#forecastH5').addClass('show');
+   $("#searchTerm").val("");  
+$("#searchTerm").keypress(function(event) { 
+if (event.keyCode === 13) { 
+  event.preventDefault();
+  $("#searchBtn").click(); 
+} 
+});
+const queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
+    $.ajax({
+    url: queryUrl,
+    method: "GET"
+  })
+  .then(function (response){
+
+    console.log(response)
+
+    console.log(response.name)
+    console.log(response.weather[0].icon)
+
+    let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+    console.log(Math.floor(tempF))
+
+    console.log(response.main.humidity)
+
+    console.log(response.wind.speed)
+
+    getCurrentConditions(response);
+    getCurrentForecast(response);
+    makeList();
+
+    })
+  });
+
+  function makeList() {
+    let listItem = $("<li>").addClass("list-group-item").text(city);
+    $(".list").append(listItem);
+  }
